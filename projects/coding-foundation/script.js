c3164a872a878800c31a71c09236ea76b95682d5
Joyce Zheng <joycezheng@JoycedeMacBook-Pro-2.local>
2020-09-15 23:11:35 +0800

    everything but working

--- projects/coding-foundation/script.js
+++ projects/coding-foundation/script.js
@@ -1,11 +1,11 @@
 let number;
-function createCat(){
-  number = document.getElementById('inputNum').value //get input value
+function beSad(){
+  number = document.getElementById('Number').value //get input value
 
   for (var i = 0; i < number; i++) {
     var ul = document.createElement('ul'); //create new cats
-    ul.className += "flex-item";
-    document.getElementById('flex').appendChild(ul);//add the cat to parent list
+    ul.className += "box-item";
+    document.getElementById('box').appendChild(ul);//add the cat to parent list
 
   }
 
