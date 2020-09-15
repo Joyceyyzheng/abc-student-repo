dfdf08be579e5b44f778c9f178a4217f6b2c8e2a
Joyce Zheng <joycezheng@JoycedeMacBook-Pro-2.local>
2020-09-15 22:18:16 +0800

    test1

--- projects/coding-foundation/script.js
+++ projects/coding-foundation/script.js
@@ -1,12 +1,12 @@
 let number;
-function Generate(){
- number = document.getElementById('Number').value //get input value
+function createCat(){
+  number = document.getElementById('inputNum').value //get input value
 
- for (var i = 0; i < number; i++) {
-   var ul = document.createElement('ul'); //create new cats
-   ul.className += "jump-item";
-   document.getElementById('jump').appendChild(ul);//add the cat to parent list
+  for (var i = 0; i < number; i++) {
+    var ul = document.createElement('ul'); //create new cats
+    ul.className += "flex-item";
+    document.getElementById('flex').appendChild(ul);//add the cat to parent list
 
- }
+  }
 
 }
