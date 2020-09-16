39d9e2039cf64e9ecdefb80e393e7de7bc744ccc
Joyce Zheng <joycezheng@JoycedeMacBook-Pro-2.local>
2020-09-16 11:29:58 +0800

    the input and the button are still not on the same horizontal line...but they are on the same vertical line now

--- projects/coding-foundation/script.js
+++ projects/coding-foundation/script.js
@@ -1,9 +1,10 @@
+
 let number;
 function beSad(){
   number = document.getElementById('Number').value //get input value
-
   for (var i = 0; i < number; i++) {
     var ul = document.createElement('ul'); //create new cats
+
     ul.className += "flex-item";//ul.className="box-item"+ul.className
     document.getElementById('flex').appendChild(ul);//add the cat to parent list
 
